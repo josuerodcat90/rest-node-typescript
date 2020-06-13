92a76dfeb09ba52f576a4bc29de74b1f94a6cacc
55036662 <zhippuden@gmail.com>
2020-06-13 18:47:02 +0200

    updated data types and methods types in routes.

--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -10,39 +10,39 @@ class PostRoutes {
 		this.routes();
 	}
 
-	async getPosts(req: Request, res: Response) {
+	public async getPosts(req: Request, res: Response): Promise<void> {
 		const posts = await Post.find();
 		res.json({ data: posts });
 	}
 
-	async getPost(req: Request, res: Response) {
+	public async getPost(req: Request, res: Response): Promise<void> {
 		const { url } = req.params;
-		const post = await Post.findOne({ url: url });
+		const post = await Post.findOne({ url });
 		res.json({ data: post });
 	}
 
-	async createPost(req: Request, res: Response) {
+	public async createPost(req: Request, res: Response): Promise<void> {
 		const { title, content, url, image } = req.body;
 		const newPost = new Post({ title, content, url, image });
 		await newPost.save();
 		res.json({ data: newPost });
 	}
 
-	async updatePost(req: Request, res: Response) {
+	public async updatePost(req: Request, res: Response): Promise<void> {
 		const { title, content, image } = req.body;
 		const { url } = req.params;
 		const updatedPost = await Post.findOneAndUpdate(
-			{ url: url },
+			{ url },
 			{ title, content, image },
 			{ new: true }
 		);
 		res.json({ data: updatedPost });
 	}
 
-	async deletePost(req: Request, res: Response) {
+	public async deletePost(req: Request, res: Response): Promise<void> {
 		const { url } = req.params;
-		await Post.findOneAndDelete({ url: url });
-		res.json({ Message: 'Post was deleted.' });
+		await Post.findOneAndDelete({ url });
+		res.json({ Message: 'Post was deleted succesfully.' });
 	}
 
 	routes() {
